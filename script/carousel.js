dec26543305ca53cf1502bae0808a128b4eae4e9
Victor Job <victorsimon553@gmail.com>
2023-08-17 18:41:54 -0300

    Adicionando carousel de imagens e ajustando layout

--- script/carousel.js
+++ script/carousel.js
@@ -1,35 +1,61 @@
-function createCarousel(container) {
-  const items = container.querySelectorAll('img'); // Get all images inside the container
-  let currentIndex = 0;
+window.addEventListener('load',()=>{
+  document.querySelector('#principal').style.transform = 'translateX(0)'
+  document.querySelectorAll('.images').forEach(containerImg=>{
+    let containerNode = containerImg.parentNode;
 
-  function showItem(index) {
-    items.forEach((item, i) => {
-      item.style.display = i === index ? 'block' : 'none';
-    });
+    let containerImage = containerNode.querySelector('.images');
+    let widthCImage = containerImage.clientWidth;
+    let images = containerImage.querySelectorAll('img');
+    for(let i = 0 ; i < images.length; i++){
+      images[i].style.minWidth = widthCImage +'px';
+      images[i].style.left = widthCImage * i + 'px' ;
+      images[i].setAttribute('transform', widthCImage * i + 'px');
+    }
+  })
+})
+
+ let idx=0;
+ let idcarousel = []
+
+
+ const handleButton = (value, e) => {
+  let containerpai = e.parentNode.parentNode;
+  console.log(containerpai, e)
+  let containerImage = containerpai.querySelector('.images');
+  let widthCImage = containerImage.clientWidth;
+  let images = containerImage.querySelectorAll('img');
+
+  let objetoIndex =  idcarousel.findIndex(obj => obj.id === containerpai.id);
+
+  if(objetoIndex !== -1){
+
+  }else{
+    idcarousel.push({id: containerpai.id, valor: 0})
+    objetoIndex =  idcarousel.findIndex(obj => obj.id === containerpai.id);
   }
 
-  function nextItem() {
-    currentIndex = (currentIndex + 1) % items.length;
-    showItem(currentIndex);
+
+  
+  if(value === 'next'){
+    idcarousel[objetoIndex].valor = idcarousel[objetoIndex].valor + 1
+    if(idcarousel[objetoIndex].valor > images.length -1){
+      idcarousel[objetoIndex].valor = 0
+      containerImage.style.transform = `translateX(0px)`
+      console.log('if next', idcarousel[objetoIndex].valor)
+    }else{
+    containerImage.style.transform = `translateX(${-idcarousel[objetoIndex].valor * widthCImage}px)`
   }
-
-  function prevItem() {
-    currentIndex = (currentIndex - 1 + items.length) % items.length;
-    showItem(currentIndex);
   }
-
-  // Show the first item initially
-  showItem(currentIndex);
-
-  // Add event listeners to the "Next" and "Previous" buttons
-  const nextBtn = container.nextElementSibling;
-  const prevBtn = container.previousElementSibling;
-  nextBtn.addEventListener('click', nextItem);
-  prevBtn.addEventListener('click', prevItem);
+  else if(value ==='prev'){
+    idcarousel[objetoIndex].valor = idcarousel[objetoIndex].valor -1; 
+     if(idcarousel[objetoIndex].valor === -1 ){
+      idcarousel[objetoIndex].valor=images.length - 1;
+      containerImage.style.transform = `translateX(${-(images.length-1) * widthCImage}px)`
+      console.log('if prev',idcarousel[objetoIndex].valor )
+    }else{
+    containerImage.style.transform = 
+    `translateX(${-idcarousel[objetoIndex].valor * widthCImage}px)`
+  }
+  
 }
-
-// Call the function for each carousel container
-document.addEventListener('DOMContentLoaded', function () {
-  const carousels = document.querySelectorAll('.image-main, .nba-Image, .whatsapp-image');
-  carousels.forEach(createCarousel);
-});
+};
